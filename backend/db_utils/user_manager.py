71bd2eee51e9f7312b74d65ff87f0e43bce90a6e
Henry <henryhuang509@gmail.com>
2023-07-24 21:37:36 +0800

    implement functions to create, read, update, delete, and list 'user'

---
+++ backend/db_utils/user_manager.py
@@ -0,0 +1,52 @@
+from models import db
+from models.user import User
+from . import argon2
+
+
+class UserManager:
+    @staticmethod
+    def create(username: str, password: str, address: str) -> bool:
+        try:
+            password_hash = argon2.generate_password_hash(password)
+            new_user = User(
+                username = username,
+                password = password_hash,
+                address = address
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def read(username: str) -> User | None:
+        user = User.query.get(username)
+        return user
+
+    @staticmethod
+    def update(username: str, password: str | None, address: str | None) -> bool:
+        user = User.query.get(username)
+        if user:
+            if password:
+                password_hash = argon2.generate_password_hash(password)
+                user.password = password_hash
+            if address:
+                user.address = address
+            db.session.commit()
+            return True
+        return False
+
+    @staticmethod
+    def delete(username: str) -> bool:
+        user = User.query.get(username)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return True
+        return False
+
+    @staticmethod
+    def list() -> list[User]:
+        users = User.query.all()
+        return users
