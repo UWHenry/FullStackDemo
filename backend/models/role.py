6ffa331a1815f57857a0e1e844f93aae0c9293da
Henry <henryhuang509@gmail.com>
2023-07-26 00:30:41 +0800

    rewrite manay to many relationship between user and role

--- backend/models/role.py
+++ backend/models/role.py
@@ -3,10 +3,10 @@ from . import db
 
 class Role(db.Model):
     __tablename__ = "roles"
-
     rolename = db.Column(db.String, primary_key=True, index=True)
     permission = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+    roles = db.relationship('User', secondary='user_roles', back_populates='roles')
     version_id = db.Column(db.Integer, nullable=False, default=0)
 
     def get_id(self):
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,19 +1,11 @@
 from . import db
 
-
-# Association Table for many-to-many relationship between user and role
-user_roles = db.Table('user_roles',
-    db.Column('user_username', db.String, db.ForeignKey('users.username'), primary_key=True),
-    db.Column('role_rolename', db.String, db.ForeignKey('roles.rolename'), primary_key=True)
-)
-
-
 class User(db.Model):
     __tablename__ = "users"
     username = db.Column(db.String, primary_key=True, index=True)
     password = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
-    roles = db.relationship('Role', secondary=user_roles, backref=db.backref('users', lazy=True))
+    roles = db.relationship('Role', secondary='user_roles', back_populates='users')
     version_id = db.Column(db.Integer, nullable=False, default=0)
 
     def get_id(self):
