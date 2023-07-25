db92e21fca159b858110050f739828c5ef1aed36
Henry <henryhuang509@gmail.com>
2023-07-26 00:29:45 +0800

    format the code

--- backend/resources/csrf_resource.py
+++ backend/resources/csrf_resource.py
@@ -1,18 +1,15 @@
-from flask_restx  import Resource, fields
-from flask import session
-from . import ns
+from flask import session, jsonify
+from flask_restx import Resource
 from flask_wtf.csrf import generate_csrf
+from namespace_models import api_ns, csrf_token_model
 
-csrf_model = ns.model("CSRF Token API Output", {
-    'csrf_token': fields.String(required=True, description='The role name')
-})
 
-@ns.route('/get_csrf_token')
+@api_ns.route('/get_csrf_token')
 class CSRFTokenResource(Resource):
-    @ns.marshal_with(csrf_model, code=200, description="Return CSRF token if exists")
+    @api_ns.marshal_with(csrf_token_model, code=200, description="Return CSRF token")
     def get(self):
         csrf_token = session.get('csrf_token')
         if not csrf_token:
             csrf_token = generate_csrf()
             session['csrf_token'] = csrf_token
-        return {'csrf_token': csrf_token}, 200
+        return jsonify({'csrf_token': csrf_token}), 200
