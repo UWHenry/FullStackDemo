a5d873749d2a78f51163462791b26e45fd3aaae0
Henry <henry@PhoenixdeMacBook-Pro.local>
2023-08-04 13:08:57 +0800

    frontend csrf

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,13 +2,27 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: "https://localhost:8000",
+    withCredentials: true
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
+    async (config) => {
+        // configure JSON Web Token
         const token = sessionStorage.getItem("jwtToken");
         if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        // configuer csrf token
+        if (['post', 'put', 'patch', 'delete'].includes(config.method.toLowerCase()) && !config.headers['X-CSRFToken']) {
+            try {
+                const response = await axios.get(
+                    `${axiosInstance.defaults.baseURL}/api/get_csrf_token`, 
+                    {withCredentials: true}
+                );
+                config.headers['X-CSRFToken'] = response.data.csrf_token;
+            } catch (error) {
+                console.error('Error fetching CSRF token:', error);
+            }
         }
         return config;
     },
