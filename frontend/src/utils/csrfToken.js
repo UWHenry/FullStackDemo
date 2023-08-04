5586625c90cbeda2615b8f73b40177b9fee8468e
Henry <henry@PhoenixdeMacBook-Pro.local>
2023-08-04 13:16:05 +0800

    move to axiosInstance

--- frontend/src/utils/csrfToken.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const getCsrfToken = async () => {
-    let response = await axios.get('https://localhost:8000/api/get_csrf_token')
-    return response.data.csrf_token
-};
-  
-export default getCsrfToken;
