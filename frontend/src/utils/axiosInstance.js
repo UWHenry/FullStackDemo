2e85d4d020fcf6878504021f5d71c4b5d92cb200
Henry <henry@wrl90-180.nchc.org.tw>
2023-08-07 16:26:56 +0800

    update csrf and jwt config

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,31 +4,30 @@ const axiosInstance = axios.create({
     baseURL: "https://localhost:8000",
     withCredentials: true
 });
+function getCookie(name) {
+    let value = `; ${document.cookie}`;
+    let parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
 axiosInstance.interceptors.request.use(
     async (config) => {
-        // configure JSON Web Token
-        const token = sessionStorage.getItem("jwtToken");
-        if (token) {
-            config.headers["Authorization"] = `Bearer ${token}`;
-        }
         // configuer csrf token
-        if (['post', 'put', 'patch', 'delete'].includes(config.method.toLowerCase()) && !config.headers['X-CSRFToken']) {
-            try {
-                const response = await axios.get(
-                    `${axiosInstance.defaults.baseURL}/api/get_csrf_token`,
-                    { withCredentials: true }
-                );
-                config.headers['X-CSRFToken'] = response.data.csrf_token;
-            } catch (error) {
-                console.error('Error fetching CSRF token:', error);
-            }
-        }
+        let csrfToken = getCookie("csrf_access_token");
+        if (csrfToken) config.headers["X-CSRF-TOKEN"] = csrfToken;
         return config;
     },
     (error) => {
-        console.log("axios error:");
-        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 422) {
+            window.location.href = '/logout';
+        }
         return Promise.reject(error);
     }
 );
